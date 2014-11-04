533a38a1942805d2940987c348fe91df101b9409
Mike Hume <mhumesf@gmail.com>
2014-11-04 03:09:40 -0700

    Syntax fixes

--- src/raster.py
+++ src/raster.py
@@ -3,7 +3,7 @@ import os
 import tempfile
 # The related files are at $GISBASE/etc/python/grass/script/*.py
 import grass.script as grass
-from grass.pygrass.modules.shortcuts import raster as r 
+from grass.pygrass.modules.shortcuts import raster as r
 from grass.pygrass.modules import Module
 from grass.pygrass.modules.shortcuts import general as g
 
@@ -20,11 +20,12 @@ class raster:
             self.tiff = tiff
             self.output = raster
             try:
-                r.external(input=self.tiff, output=self.output) 
+                r.external(input=self.tiff, output=self.output)
             except:
                 print("r.external failed to generate raster file "
                     + "with specified input/output names "
-                    + self.tiff + " and " + self.output
+                    + self.tiff + " and " + self.output)
+
             try:
                 g.region(rast=self.output)
             except:
@@ -35,8 +36,8 @@ class raster:
 
 
     """
-    This exports the raster files using the command r.external and 
-    g.region. It takes two arguments, the first is a 
+    This exports the raster files using the command r.external and
+    g.region. It takes two arguments, the first is a
     """
     """
     def export(self, ):
@@ -58,8 +59,8 @@ class raster:
     """
     This method calls the r.slope.aspect function from the grass module.
     It takes three arguments, the first of which is an input elevation raster
-    file, the second is an output name for a slope file and the third is an 
-    output name for an aspect file. 
+    file, the second is an output name for a slope file and the third is an
+    output name for an aspect file.
     """
     def slopeAspect(self, elevRaster, outputSlope, outputAspect):
         try:
@@ -75,13 +76,13 @@ class raster:
     This method calls the r.sun function from the grass module. There are a
     ton of arguments for it to run properly.
     """
-    def rSun(self, myElevRaster, mySlope, myAspect, myDay, myStep, myBeam_rad, 
+    def rSun(self, myElevRaster, mySlope, myAspect, myDay, myStep, myBeam_rad,
              myInsol_time, myDiff_rad, myRefl_rad, myGlob_rad):
         try:
             # call r.sun
 	        r.sun(elevationRaster=MyElevRaster, slope=mySlope,
-	          aspect=myAspect, day=myDay step=myStep, declin="0"    
-	          dist="1", beam_rad=myBeam_rad, insol_time=myInsol_time, 
+	          aspect=myAspect, day=myDay step=myStep, declin="0"
+	          dist="1", beam_rad=myBeam_rad, insol_time=myInsol_time,
 		      diff_rad=myDiff_rad, refl_rad=myRefl_rad,
 		      glob_rad=myGlob_rad flags="s" overwrite=true)
         except:
@@ -99,18 +100,18 @@ class raster:
 		        lapseRate = 5.69
 		        tmin = paramRaster
 		        r.mapcalc( "%s = %s-(%f/1000*(%s-%s))" % rasterOut,
-		                  tmin, lapseRate, elevRaster, 
+		                  tmin, lapseRate, elevRaster,
 		                  daymetRaster )
 	        if param == "tmax":
 		        lapseRate = 5.69 # is this the same for tmax?
 		        tmax = paramRaster
 		        r.mapcalc( "%s = %s-(%f/1000*(%s-%s))" % rasterout,
-		                  tmax, lapseRate, elevRaster, 
+		                  tmax, lapseRate, elevRaster,
 		                  daymetRaster )
 	        else:
 		        print "Invalid param type"
-	
-	        #TODO: Add the rest of the equations for each parameter
+
+	        # TODO: Add the rest of the equations for each parameter
 
     #def rPatch(self):
 
