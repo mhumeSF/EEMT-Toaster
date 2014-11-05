62fb9a4912f495168ebec57ca5ec4b16db17880e
dspence84 <dspence84@email.arizona.edu>
2014-11-04 17:02:21 -0700

    updated raster.py to use call() instead of pygrass

--- src/raster.py
+++ src/raster.py
@@ -2,10 +2,7 @@ import sys
 import os
 import tempfile
 # The related files are at $GISBASE/etc/python/grass/script/*.py
-import grass.script as grass
-from grass.pygrass.modules.shortcuts import raster as r
-from grass.pygrass.modules import Module
-from grass.pygrass.modules.shortcuts import general as g
+from subprocess import call
 
 class raster:
 
@@ -20,14 +17,16 @@ class raster:
             self.tiff = tiff
             self.output = raster
             try:
-                r.external(input=self.tiff, output=self.output)
+                #r.external(input=self.tiff, output=self.output)
+				call(["r.external", "input=%s" % (self.tiff), "output=%s" % (self.output) ])
             except:
                 print("r.external failed to generate raster file "
                     + "with specified input/output names "
                     + self.tiff + " and " + self.output)
 
             try:
-                g.region(rast=self.output)
+				#g.region(rast=self.output)
+				call(["g.region", "rast=%s" % (self.output) ])
             except:
                 print("g.region failed to generate output raster file "
                     + "with specified name: " + self.output)
@@ -37,23 +36,19 @@ class raster:
 
     """
     This exports the raster files using the command r.external and
-    g.region. It takes two arguments, the first is a
+    g.region. It takes two arguments, the first is a raster and the second
+	is the name of the geoTiff to output as.
     """
-    """
-    def export(self, ):
-        for arg in sys.argv:
-		    mySplit = arg.split('=')
-		    if len(mySplit) > 1:
-			    command = mySplit[0]
-			    value = mySplit[1]
-			    if command = "input":
-				    myInput = value
-			    if command = "output":
-				    myOutput = value
-
-        r.external(input=myInput, output=myOutput)
-        g.region(rast=myOutput)
-    """
+    
+    def export(self, raster, output):
+		try:
+			call(["r.out.gdal", "input=%s" % (raster), "output=%s" % (output)])
+		except:
+			print("r.in.gdal failed to generate your geoTif")
+        
+		#r.external(input=myInput, output=myOutput)
+        
+    
 
 
     """
@@ -62,12 +57,13 @@ class raster:
     file, the second is an output name for a slope file and the third is an
     output name for an aspect file.
     """
-    def slopeAspect(self, elevRaster, outputSlope, outputAspect):
+    def slopeAspect(self, elevationRaster, outputSlope, outputAspect):
         try:
             # call r.slope.aspect
-            slope_aspect = Module("r.slope.aspect")
-            slope_aspect(elevation=elevationRaster, slope=outputSlope,
-                         aspect=outputAspect, format='degrees', overwrite=True)
+            #slope_aspect = Module("r.slope.aspect")
+            #slope_aspect(elevation=elevationRaster, slope=outputSlope,
+            #             aspect=outputAspect, format='degrees', overwrite=True)
+			call(["r.slope.aspect", "elevation=%s" % (elevationRaster), "slope=%s" % (mySlope), "aspect=%s" % (myAspect), "format=degrees"])
         except:
             print("Failed to run r.slope.aspect with specified arguments")
 
@@ -80,11 +76,19 @@ class raster:
              myInsol_time, myDiff_rad, myRefl_rad, myGlob_rad):
         try:
             # call r.sun
-	        r.sun(elevationRaster=MyElevRaster, slope=mySlope,
-	          aspect=myAspect, day=myDay, step=myStep, declin="0",
-	          dist="1", beam_rad=myBeam_rad, insol_time=myInsol_time,
-		      diff_rad=myDiff_rad, refl_rad=myRefl_rad,
-		      glob_rad=myGlob_rad, flags="s", overwrite=true)
+	        #r.sun(elevationRaster=MyElevRaster, slope=mySlope,
+	        #  aspect=myAspect, day=myDay, step=myStep, declin="0",
+	        #  dist="1", beam_rad=myBeam_rad, insol_time=myInsol_time,
+		    #  diff_rad=myDiff_rad, refl_rad=myRefl_rad,
+		    #  glob_rad=myGlob_rad, flags="s", overwrite=true)
+			
+			call(["r.sun", "elevin=%s" % (myElevationRaster), 
+			      "slopein=%s" % (mySlope), "aspin=%s" % (myAspect), 
+			      "day=%s" % (myDay), "step=%s" % (myStep), "declin=0", 
+			      "dist=1", "-s", "beam_rad=%s" % (myBeam_rad), 
+				  "insol_time=%s" % (myInsol_time), "diff_rad=%s" % (myDiff_rad), 
+				  "refl_rad=%s" % (myRefl_rad), "glob_rad=%s" % (myGlob_rad), 
+				  "--overwrite"])
         except:
             print("r.sun failed to run with specified arguments, specifics "
                 + "unknown since there are so many freakin arguments!")
@@ -99,15 +103,13 @@ class raster:
 	        if param == "tmin":
 		        lapseRate = 5.69
 		        tmin = paramRaster
-		        r.mapcalc( "%s = %s-(%f/1000*(%s-%s))" % rasterOut,
-		                  tmin, lapseRate, elevRaster,
-		                  daymetRaster )
+		        
+				call(["r.mapcalc", "%s = %s-(%f/1000*(%s-%s))" % (rasterout, tmin, lapseRate, elevationRaster, daymetRaster) ])
 	        if param == "tmax":
 		        lapseRate = 5.69 # is this the same for tmax?
 		        tmax = paramRaster
-		        r.mapcalc( "%s = %s-(%f/1000*(%s-%s))" % rasterout,
-		                  tmax, lapseRate, elevRaster,
-		                  daymetRaster )
+		      
+				call(["r.mapcalc", "%s = %s-(%f/1000*(%s-%s))" % (rasterout, tmax, lapseRate, elevationRaster, daymetRaster) ])
 	        else:
 		        print "Invalid param type"
 
