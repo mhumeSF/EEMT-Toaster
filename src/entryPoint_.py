0966c347dd3199d7a5cf09b1aef4386de14b8465
Mike Hume <mhumesf@gmail.com>
2014-11-11 02:06:23 -0700

    My entrypoint

--- src/entryPoint_.py
+++ src/entryPoint_.py
@@ -7,13 +7,30 @@ import os, getopt, sys, argparse
 def main():
     parser=argparse.ArgumentParser(description='''My Description. And what a lovely description it is. ''')
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
-    parser.add_argument('bar', nargs='*', default=[1, 2, 3], help='BAR!')
+
+    parser.add_argument('-t', '--geotiff', type=str, help='geotiff or folder of geotiffs', required=True)
+    parser.add_argument('-s', '--start', type=str, help='Enter a start year [Default 1980]')
+    parser.add_argument('-e', '--end', type=str, help='Enter an end year [Default 2013]')
 
     if len(sys.argv)==1:
+        parser.print_help()
         parser.print_help()
         sys.exit(1)
 
     args=parser.parse_args()
+
+    print(args)
+
+    startYear = args.start
+    endYear= args.end
+
+    #if(os.path.isdir(args.geotiff)):
+        #geotiff = []
+        #for file in os.listdir(args.geotiff):
+            #if file.endswith("*.tif"):
+                #geotiff.append(file)
+
+    #print startYear + ' ' + endYear
 
 if __name__ == "__main__":
     main()
