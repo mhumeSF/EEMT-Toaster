8faa13d1c36947f979bf2810adee57292c23275a
Daniel Bryan Spence <dspence@service1.ice.americas.sgi.com>
2014-12-08 19:17:45 -0700

    changed geotiff to work with hpc python version

--- src/geotiff.py
+++ src/geotiff.py
@@ -12,7 +12,7 @@ class geotiff:
         if not tiff:
             tiff = raw_input("Input path to geotiff file: ")
         try:
-            self.tiff = tiff
+            self.tiff = tiff 
         except:
             print("No valid geotiff file specified, exiting...")
 
@@ -28,61 +28,57 @@ class geotiff:
         #generate command for os
         command = ("gdalinfo " + self.tiff)
         #try to run gdalinfo command
-        try:
-            info = subprocess.check_output(command, shell = True)
-            
-            # This is a static set of lat/longs for testing purposes
-            # info = "Upper Left  (  321240.000, 4106000.000) (119d 0'40.18\"W, 37d 4'59.71\"N) \n Lower Right (  526000.000, 2811000.000) 			( 80d44'29.28\"W, 25d24'56.39\"N)"
-            #search for center coordinates in output of gdalinfo
-            UL = re.search('Upper Left(.*)', info)
-            LR = re.search('Lower Right(.*)', info)
+        #info = subprocess.check_output(command, shell = True)
+        info = os.popen(command).read()   
+        # This is a static set of lat/longs for testing purposes
+        # info = "Upper Left  (  321240.000, 4106000.000) (119d 0'40.18\"W, 37d 4'59.71\"N) \n Lower Right (  526000.000, 2811000.000) 			( 80d44'29.28\"W, 25d24'56.39\"N)"
+        #search for center coordinates in output of gdalinfo
+        UL = re.search('Upper Left(.*)', info)
+        LR = re.search('Lower Right(.*)', info)
+        #if center is found
+        if UL and LR:
+            #save match as string
+            UL_str = UL.group()
+            LR_str = LR.group()
+            #make list of results
+            UL = UL_str.split('(')
+            LR = LR_str.split('(')
+            #if the list has 3 elements (assuming proper gdalinfo output format)
+            if len(UL) == 3 and len(LR) == 3:
+                #save coordinate values(assumed to be 3rd item)
+                UL = UL[2]
+                LR = LR[2]
+                #split them on the comma
+                UL = UL.split(',')
+                LR = LR.split(',')
+                # print (UL, LR)
 
-            #if center is found
-            if UL and LR:
-                #save match as string
-                UL_str = UL.group()
-                LR_str = LR.group()
-                #make list of results
-                UL = UL_str.split('(')
-                LR = LR_str.split('(')
-                #if the list has 3 elements (assuming proper gdalinfo output format)
-                if len(UL) == 3 and len(LR) == 3:
-                    #save coordinate values(assumed to be 3rd item)
-                    UL = UL[2]
-                    LR = LR[2]
-                    #split them on the comma
-                    UL = UL.split(',')
-                    LR = LR.split(',')
+                #if the length of the resulting list is 2
+                if len(UL) == 2 and len(LR) == 2:
+                    #save lat/lon values in tuple values (lat,lon)
+                    ULlon = UL[0].lower().strip()
+                    ULlat = UL[1][:-1].lower().strip()
+                    LRlon = LR[0].lower().strip()
+                    LRlat = LR[1][:-1].lower().strip()
 
-                    # print (UL, LR)
-
-                    #if the length of the resulting list is 2
-                    if len(UL) == 2 and len(LR) == 2:
-                        #save lat/lon values in tuple values (lat,lon)
-                        ULlon = UL[0].lower().strip()
-                        ULlat = UL[1][:-1].lower().strip()
-                        LRlon = LR[0].lower().strip()
-                        LRlat = LR[1][:-1].lower().strip()
-
-                        #append (-)'s for S and W lon/lat values
-                        #remove N,S,E,W letters from strings                        
-                        for coord in [ULlon, ULlat, LRlon, LRlat]:
-                            if coord[-1] in ['s', 'w']:
-                                coord = '-' + coord[:-1]
-                            else:
-                                coord = coord[:-1]
+                    #append (-)'s for S and W lon/lat values
+                    #remove N,S,E,W letters from strings                        
+                    for coord in [ULlon, ULlat, LRlon, LRlat]:
+                        if coord[-1] in ['s', 'w']:
+                            coord = '-' + coord[:-1]
+                        else:
+                            coord = coord[:-1]
                         
-                        #return the longitude and latitude values as tuple
-                        return ((ULlat, ULlon), (LRlat, LRlon))
+                    #return the longitude and latitude values as tuple
+                    return ((ULlat, ULlon), (LRlat, LRlon))
 
-                    else:
-                        print("Improper lon/lat format in geotiff file")
                 else:
-                    print("Improper center coordinate format in geotiff file")
+                    print("Improper lon/lat format in geotiff file")
             else:
-                print("Could not find center coordinates in gdal output")
-        except:
-            print("Invalid geotiff file")
+                print("Improper center coordinate format in geotiff file")
+        else:
+            print("Could not find center coordinates in gdal output")
+
 
 
     def toDegrees(self, coords):
@@ -106,8 +102,10 @@ class geotiff:
                 #print(command) #for debugging
                 try:
                     #run geoconvert to get UTM value
-                    ULutm = subprocess.check_output(ULcommand, shell=True)
-                    LRutm = subprocess.check_output(LRcommand, shell=True)
+                    #ULutm = subprocess.check_output(ULcommand, shell=True)
+                    #LRutm = subprocess.check_output(LRcommand, shell=True)
+                    ULutm = os.popen(ULcommand).read()
+                    LRutm = os.popen(LRcommand).read()
                     return (ULutm.strip(), LRutm.strip())
                 except:
                     print("Improper geoconvert command, exiting...")
