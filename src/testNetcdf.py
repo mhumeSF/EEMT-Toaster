06fdbf4b5b0e86c05923286ae348a1ea5ac60e84
Daniel Spence <dspence@email.arizona.edu>
2014-12-09 04:24:50 -0700

    added images

--- src/testNetcdf.py
+++ src/testNetcdf.py
@@ -5,13 +5,15 @@ import sys, os
 
 
 
+n = netcdf(1980, [11369, 11370], "tmin")
 
-n = netcdf(1980, [11369,11370], "tmin")
 
-print n.patchRaster
+
 
 print "now testing a single raster area"
 
 n = netcdf(1980, [11371], "tmax")
+n = netcdf(1980, [11371], "tmin")
+n = netcdf(1980, [11371], "prcp")
 
 print n.patchRaster
