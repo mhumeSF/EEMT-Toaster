66b8b45bd55e40dafa9e75aeefb98dd4af119f39
Mike Hume <mhumesf@gmail.com>
2014-12-13 16:18:10 -0700

    Comments added

--- src/verify.py
+++ src/verify.py
@@ -2,18 +2,22 @@
 
 import urllib2, os, sys
 
+# Get URL and Filename
 url = sys.argv[1]
 filename = url.split('/')[-1]
 
-req_info = urllib2.urlopen('http://uanow.org/ISTA-420-Midterm/gooey/index.html')
+# Get 'content-length' header from URL
+req_info = urllib2.urlopen(url)
 req_info.headers.keys()
-
 contentLength = req_info.headers['content-length']
 
+# Get filesize of file
 fileSize = os.path.getsize(filename)
 
 print "Length: " + str(contentLength)
 print "Size: " + str(fileSize)
+
+# Compare filesize to content-lenght 'header'
 if(int(contentLength) == int(fileSize)):
     print "Hoo-ray!"
 else:
