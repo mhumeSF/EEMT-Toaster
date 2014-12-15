f9dd7c869ce51bdf4fb5240d3041b64ceeba9e93
Daniel Spence <dspence@email.arizona.edu>
2014-12-15 11:30:55 -0700

    added a testGrass.py and fixed some issue with netcdfAverage.py

--- src/grassData.py
+++ src/grassData.py
@@ -2,6 +2,4 @@
 
 class grassData:
     grassdataFolder = "grassData"
-    rmapOutputFolders = ["cats", "cell", "cellhd", \
-						 "cell_misc", "color", "fcell" \
-						 "hist"]
+    rmapOutputFolders = ["cats", "cell", "cellhd", "cell_misc", "color", "fcell", "hist"]
