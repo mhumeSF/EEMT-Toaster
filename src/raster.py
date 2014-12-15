0a3127cdec76fa9cd4f14733e52adf41b9f27710
Daniel Spence <dspence@email.arizona.edu>
2014-12-14 18:53:13 -0700

    changed gdalwarp in raster.py

--- src/raster.py
+++ src/raster.py
@@ -20,7 +20,7 @@ class raster:
 
             try:
                 #r.external(input=self.tiff, output=self.output)
-                os.system("gdalwarp -overwrite -s_srs EPSG:26911 -t_srs \"+proj=lcc +lat_1=25 +lat_2=60 +lat_0=42.5 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs\" -tr 10 10 -r bilinear -multi -dstnodata 0 -of GTiff %s %s" % (tiff, warped))
+                os.system("gdalwarp -overwrite -t_srs \"+proj=lcc +lat_1=25 +lat_2=60 +lat_0=42.5 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs\" -tr 10 10 -r bilinear -multi -dstnodata 0 -of GTiff %s %s" % (tiff, warped))
                 call(["r.external", "input=%s" % (warped), "output=%s" % (self.output), "-o", "--overwrite" ])
 
             except:
