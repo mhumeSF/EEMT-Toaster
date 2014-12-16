001d84d55f59af605cb187e46bba8d497a5b1695
Mike Hume <mhumesf@gmail.com>
2014-12-15 20:39:34 -0700

    myVerify funtionality appended to netcdf average

--- src/netcdfAverage.py
+++ src/netcdfAverage.py
@@ -1,6 +1,6 @@
 #!usr/bin/python2
 
-import os, sys
+import os, sys, urllib2
 from workQ import *
 from grassData import *
 
@@ -28,7 +28,7 @@ class netcdf:
 
     def process (self, years, tiles, param):
         """
-        This method takes three arguments, list of years, a list of tiles, and a parameter. 
+        This method takes three arguments, list of years, a list of tiles, and a parameter.
         It then calls all of the instance methods on the current object. It is a main method of sorts.
         """
         self.years = years
@@ -40,62 +40,74 @@ class netcdf:
         for tile in tiles:
             for year in years:
                 self.maps.append( "netcdfs/" + str(param) + "_" + str(year) + "_" + str(tile) )
-                
+
                 if len(self.tiles) > 1:
                     self.rasters.append( str(param) + "_" + str(year) + "_" + str(tile) )
                 else:
                     self.rasters.append( str(param) + "_" + str(year) )
-        
+
         print self.rasters
         #set the projection and clear up g:
         command = "g.proj -c proj4=\"+proj=lcc +lat_1=25 +lat_2=60 +lat_0=42.5 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs\""
         os.system(command)
         #clear up
         # os.system("g.mremove \"*\"")
-        
+
         self.getNetcdf()
 
     def getNetcdf(self):
         """
-        This method uses wget to acquire netCdf files from the thredds file server. It gets a netCdf for each tile 
+        This method uses wget to acquire netCdf files from the thredds file server. It gets a netCdf for each tile
         and downloads them all into the local path. If a file with the same name is found in the current directory,
         downloading of the file is skipped assuming that the file has already been downloaded.
         """
         print ("saving in directory: " + os.getcwd())
-        for tile in self.tiles: 
+        for tile in self.tiles:
             for year in self.years:
-                filename = "netcdfs/" + self.param + "_" + str(year) + "_" + str(tile) + ".nc" 
-                command = ("wget -O %s http://thredds.daac.ornl.gov/thredds/fileServer/ornldaac/1219/tiles/%d/%d_%d/%s.nc") % \
-                        (filename, year, tile, year, self.param)
+                filename = "netcdfs/" + self.param + "_" + str(year) + "_" + str(tile) + ".nc"
 
-                # try: 
-                if os.path.isfile (filename):
+                url = ("http://thredds.daac.ornl.gov/thredds/fileServer/ornldaac/1219/tiles/%d/%d_%d/%s.nc") % \
+                    (year, tile, year, self.param)
+
+                verified = netcdfExists(url)
+
+                command = ("wget -O %s %s") % (filename, url)
+
+                # try:
+                if os.path.isfile (filename) and verified:
                     print filename + ": File Exists. Skipping Download..."
                 else:
-                    # os.system(command) 
+                    # os.system(command)
                     # arg1 -> tag_name
                     # arg2 -> list of strings : [command, num_inputs, <input file names>, num_outputs, <output file names>
                     taskid = self.wq.wq_job(self.tag_name, [command, "0", "1", filename])
                     self.taskids.append(taskid)
-                # except: 
+                # except:
                 #     print "netcdf command did not complete"
 
+    def netcdfExists(url):
+        """
+        Verifies netCDF is downloaded to it's entirety by verifying the filesize with the content-length header
+        """
+        return int(os.path.getsize(os.path.basename(url))) == int(urllib2.urlopen(url).headers['content-length'])
+
+
     def averageRasters(self):
-        
+
         self.taskids = []
         self.toRaster()
         if len(self.tiles) > 1:
             self.rasterPatch()
         else:
             self.patchRaster = self.rasters[0]
-        
+
         self.tag_name = "netcdf_average"
-        os.system("g.region rast=" + self.param + "." + str(self.days[0])) 
+        os.system("g.region rast=" + self.param + "." + str(self.days[0]))
 
         outputRasters = []
         inputRasters = []
         for day in self.days:
-            for folder in grassData.rmapOutputFolders: 
+            for folder in grassData.rmapOutputFolders:
                 outputRaster = grassData.grassdataFolder + "/" + folder + "/" + self.param + "." + str(day)
                 outputRasters.append(outputRaster)
             inputRaster = self.param + "." + str(day)
@@ -104,7 +116,7 @@ class netcdf:
             once = True
             for year in self.years:
                 raster = self.param + "_" + str(year)
-                
+
                 for folder in grassData.rmapOutputFolders:
                     if once:
                         inputRaster = grassData.grassdataFolder + "/" + folder + "/" + raster
@@ -116,7 +128,7 @@ class netcdf:
                 else:
                     command += "+" + raster + "." + str(day)
             command += ")/" + str(len(self.years)) + "\""
-                        
+
             wq_command = []
             wq_command.append(command)
             wq_command.append(str(len(inputRasters)))
@@ -127,11 +139,11 @@ class netcdf:
             for item in outputRasters:
                 wq_command.append(item)
 
-            #taskid = self.wq.wq_job(self.tag_name, wq_command) 
+            #taskid = self.wq.wq_job(self.tag_name, wq_command)
             taskid = self.wq.wq_job(self.tag_name, [command, "0", "0"])
             self.taskids.append(taskid)
             # os.system(command)
-            
+
     def rasterPatch(self):
         """
         This method uses r.patch to patch all the raster files together.
@@ -142,26 +154,26 @@ class netcdf:
         rastersWithDay = []
         for raster in self.rasters:
             rastersWithDay.append(raster + ".1")
-        
+
         myRegionInput = ",".join(rastersWithDay)
-        
+
         command = "g.region rast=" + myRegionInput
-        
+
         try:
             os.system(command)
         except:
             print "rasterPatch command did not complete"
-        
+
         # iterate through all days of the year
         for i in self.days:
             for year in self.years:
                 rasterWithDay = []
                 for raster in self.rasters:
                     rasterWithDay.append( raster + "." + str(i) )
-                
+
                 myRasterInput = ",".join(rasterWithDay)
                 myRasterOutput = str(self.param) + "_" + str(year)
-                
+
                 command = "r.patch input=" + myRasterInput + " output=" + myRasterOutput + "." + str(i) + " --overwrite"
                 #print command
                 #patched raster outputs will be "param_year.day"
@@ -197,7 +209,7 @@ class netcdf:
             except:
                 print "toRaster command did not complete"
         #gdal_translate -of GTiFF NETCDF:tmin.nc -b 365 myAwesomeNetCDF
-        return 
+        return
 
     # netcdf_raster_year_day_a
     # netcdf_raster_year_day_b
