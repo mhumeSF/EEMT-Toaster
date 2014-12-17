22accb2ce7711ffe0f9ea8c7bde34d5a501ec785
mhumesf <mhumesf@gmail.com>
2014-12-17 04:57:17 -0700

    some inline comments

--- src/starthere.py
+++ src/starthere.py
@@ -35,8 +35,8 @@ def main ():
     wq = workQ()
     nc_objs = []
 
-    print "r.sun"
     for tiff in tiff_files:
+        # Find the tile list for a partifular tiff file
         print ("Processing: " + tiff)
         locn = geotiff(tiff)
         coords = locn.getCoordinates()
@@ -44,50 +44,63 @@ def main ():
         tile_list = locn.getTiles (degrees)
         print tile_list
 
+        # Rasterize Topographic Wetness Index
         twiRaster = "twiRaster"
         twi_r = raster(twi_file, twiRaster, wq)
 
+        # Rasterize the dem and distribute slopeaspect
         demRaster = "demRaster"
         r = raster(tiff, demRaster, wq)
         slope = "slope"
         aspect = "aspect"
         r.slopeAspect(demRaster, slope, aspect)
 
+        # Prepare params for r.sun
         myStep = "0.05"
         sun_hours = "sun_hours"
         total_sun = "total_sun"
 
+        # Distribute r.sun and wait for it before eemt calculations
         for day in range(1,20):
             insol_time = sun_hours + "." + str(day)
             glob_rad = total_sun + "." + str(day)
             r.sun(demRaster, slope, aspect, str(day), myStep, insol_time, glob_rad)
 
+        # Now, distribute the downloading of netcdf files
+        # Each tile per param is handled as a separate netcdf object
         for param in params_to_use:
             nc = netcdf(wq, param)
             nc_objs.append(nc)
             nc.process(years, tile_list)
 
+        # Wait for netcdf files to be downloaded by the workers
         print "Downloading files . . ."
         for nc in nc_objs:
             wq.wq_wait(nc.get_tag_name(), nc.get_taskids())
         print "Completed Downloading files"
 
+        # Start working on averaging. distribute. Mercilessly
         for nc in nc_objs:
             nc.averageRasters() # cues up more jobs in the nc object -> another wait must be executed
 
-        print "Converting to Rasters . . ."
+        # Wait.. Wait... W a i t . . . .
+        print "Waiting on Converting to Rasters . . ."
         wq.wq_wait ("netcdf_toRaster", nc.get_taskids())
         print "Completed converting to rasters!"
 
-        print "Averaging rasters . . ."
+        # Wait.. Wait... W a i t . . . .
+        print "Waiting on Averaging rasters . . ."
         wq.wq_wait(nc.get_tag_name(), nc.get_taskids())
         print "Completed averaging rasters"
 
-        print "Calculating r.sun . . ."
+        # Wait.. Wait... W a i t . . . .
+        print "Waiting on Calculating r.sun . . ."
         wq.wq_wait(r.get_tag_name(), r.get_taskids())
         print "Completed r.sun"
 
-"""
+        # Welcome to the (hopefully) the last part of this project
+        # EEMT calculations and distribution processes to follow
+
         taskids = []
         for day in range(1,2):
             command = "python eemt.py %s %s %s %s %s %s %s %s %s" % (demRaster, twiRaster, dem_1km, "tmin", "tmax", "prcp", total_sun, sun_hours, day)
@@ -98,6 +111,7 @@ def main ():
         print "calculating EEMT"
         wq.wq_wait("eemt_toaster", taskids)
 
+"""
         #and we're kind of done
 
     # calculate S_i
